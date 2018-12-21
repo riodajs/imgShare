bf8f1e230932d9b50fab909a0db51fb2afe00791
riodajs <riodajs@gmail.com>
2018-12-21 17:22:11 -0300

    Add image upload

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,9 +1,21 @@
+const { randomNumber } = require('../helpers/lib');
+const path = require('path');
+const fs = require('fs-extra');
 const ctrl = {};
 
 ctrl.index = (req, res) => {
 };
 
-ctrl.create = (req, res) => {
+ctrl.create = async (req, res) => {
+    const imgUrl = randomNumber();
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const imageTempPath = req.file.path;
+    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+    console.log(req.file);
+    if(ext === '.jpg' || ext === '.png' || ext === '.svg' || ext === '.jpeg' || ext === '.gif'){
+        await fs.rename(imageTempPath, targetPath);
+    }
+    res.send('funciona');
 };
 
 ctrl.like = (req, res) => {
