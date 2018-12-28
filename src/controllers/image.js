bb6bbcbf7ff79602a87e81a20d4a114567651578
riodajs <riodajs@gmail.com>
2018-12-28 15:05:59 -0300

    Added comment form

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,8 +1,8 @@
 const { randomNumber } = require('../helpers/lib');
 const path = require('path');
 const fs = require('fs-extra');
-const { Image } = require('../models');
-
+const { Image, Comment } = require('../models');
+const md5 = require('md5');
 const ctrl = {};
 
 ctrl.index = async (req, res) => {
@@ -43,7 +43,18 @@ ctrl.create = (req, res) => {
 ctrl.like = (req, res) => {
 };
 
-ctrl.comment = (req, res) => {
+ctrl.comment = async (req, res) => {
+    const image = await Image.findOne({filename: {$regex: req.params.image_id}});
+    console.log(image);
+    if(image){
+        const newComment = new Comment(req.body);
+        newComment.gravatar = md5(newComment.email);
+        newComment.image_id = image.image_id;
+        await newComment.save();
+        res.redirect('/images/' + image.uniqueId);
+    }else{
+        res.send('no funciona');
+    }
 };
 
 ctrl.remove = (req, res) => {
